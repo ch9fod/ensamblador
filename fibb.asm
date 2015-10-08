3cd7b3cce52af9f039c3f31ac2a2debbe0a98a9e
ch9fod <ch9fod@gmail.com>
2015-10-08 10:40:18 -0500

    update
    update

--- fibb.asm
+++ fibb.asm
@@ -5,7 +5,11 @@
 mov dh,0h
 mov dl,01h
 call imprimeChar
-mov cl,10
+push dx ;--------------
+mov dl,20h  ;--------------
+call imprimeHex ;--------------
+pop dx  ;--------------
+mov cl,11
 L1:
 call calcula
 call imprime
@@ -19,23 +23,28 @@ mov dh,ch
 ret
 imprime:
 push dx
+and ah,0    ;and con 0 = 0--------------
 mov al,dl
 mov bl,100
 div bl
+cmp al,0  ;compara si es cero----------------
 jz L2
 mov dl,al
 call imprimeChar
 L2:
 xchg al,ah
-and ah,0    ;and con 0 = 0
+and ah,0    ;and con 0 = 0---------------
 mov bl,10
 div bl
+cmp al,0  ;compara si es cero-------------
 jz L3
 mov dl,al
 call imprimeChar
 L3:
 mov dl,ah
 call imprimeChar
+mov dl,20h  ;------------------
+call imprimeHex ;------------
 pop dx
 ret
 imprimeChar:
@@ -46,6 +55,12 @@ mov ah,02h
 int 21h
 pop ax
 pop dx
+ret
+imprimeHex: ;---------------
+push ax
+mov ah,02h
+int 21h
+pop ax
 ret
 
 fin:
