e790abb346a47496e99010dc745cb020f64b77af
ch9fod <ch9fod@gmail.com>
2015-11-22 23:28:04 -0600

    50x50
    dibuja cuadro de 50x50

--- int10.asm
+++ int10.asm
@@ -17,6 +17,59 @@ ory		dw ? ;origen y
 finx	dw 0 ;fin x
 finy	dw 0 ;fin y
 color	db ? ;que color?
+imagen1x dw 50	;pixeles de imagen1
+imagen1y dw 50	;pixeles de imagen1
+imagen1 db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+		db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+		db 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
+		db 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3
+		db 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
+		db 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
+		db 6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6
+		db 7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
+		db 8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+		db 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9
+		db 10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10
+		db 11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11
+		db 12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12
+		db 13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13
+		db 14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14
+		db 15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15
+		db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+		db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+		db 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
+		db 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3
+		db 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
+		db 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
+		db 6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6
+		db 7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
+		db 8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+		db 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9
+		db 10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10
+		db 11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11
+		db 12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12
+		db 13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13
+		db 14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14
+		db 15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15
+		db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+		db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+		db 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
+		db 3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3
+		db 4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4
+		db 5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5
+		db 6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6
+		db 7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7
+		db 8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8
+		db 9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9
+		db 10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10
+		db 11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11
+		db 12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12
+		db 13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13
+		db 14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14
+		db 15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15
+		db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+		db 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+		db 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
 .code
 .startup
 main proc
@@ -39,61 +92,61 @@ int 10h
 ;http://stanislavs.org/helppc/int_10-b.html
 
 ;------------------------------------------------------
-jmp leepixel		;salto para debug
+jmp dibujaimagen		;salto para debug
 ;------------------------------------------------------
 
-xor cx,cx			;clear
-xor dx,dx			;clear
-xor ax,ax			;clear
+; xor cx,cx			;clear
+; xor dx,dx			;clear
+; xor ax,ax			;clear
 
-lea bx,cuadro1		;dir de registro inicial cuadro1
-call drawcuad		;rutina de dubujar cuadro
+; lea bx,cuadro1		;dir de registro inicial cuadro1
+; call drawcuad		;rutina de dubujar cuadro
 
-lea bx,cuadro2		;dir de registro inicial cuadro1 
-call drawcuad		;rutina de dubujar cuadro
+; lea bx,cuadro2		;dir de registro inicial cuadro1 
+; call drawcuad		;rutina de dubujar cuadro
 
-lea bx,cuadro3		;dir de registro inicial cuadro1
-call drawcuad		;rutina de dubujar cuadro
+; lea bx,cuadro3		;dir de registro inicial cuadro1
+; call drawcuad		;rutina de dubujar cuadro
 
-jmp fin 			
+; jmp fin 			
 
-drawcuad:
-mov al,[bx] 		;toda esta seccion
-push ax				;
-and al,0fh			;lo unico que hace
-mov color,al		;
-pop ax				;es sacar los valores
-inc bx				;
-inc bx				;en memoria
-mov cx,[bx]			;
-mov orx,cx			;de el origen en x
-mov finx,cx			;
-inc bx				;y el origen en y
-inc bx				;
-mov cx,[bx]			;y tambien calcula
-mov ory,cx			;
-mov finy,cx			;los puntos finales
-inc bx				;
-inc bx				;en x Y y
-mov cx,[bx]			;
-add finx,cx			;
-add finy,cx			;
-xor cx,cx			;clear
-xor dx,dx			;clear
-mov cx,orx			;las coordenadas a dibujar deben estar en
-mov dx,ory			;cx y dx 
-dibuja:
-call pixel 			;dibuja el pixel del color indicado
-inc cx				;incrementa la direccion a dibujar en x
-cmp cx,finx
-jne dibuja
-mov cx,orx
-inc dx				;incrementa la direccion a dibujar en y
-cmp dx,finy
-jne dibuja
-ret
+; drawcuad:
+; mov al,[bx] 		;toda esta seccion
+; push ax				;
+; and al,0fh			;lo unico que hace
+; mov color,al		;
+; pop ax				;es sacar los valores
+; inc bx				;
+; inc bx				;en memoria
+; mov cx,[bx]			;
+; mov orx,cx			;de el origen en x
+; mov finx,cx			;
+; inc bx				;y el origen en y
+; inc bx				;
+; mov cx,[bx]			;y tambien calcula
+; mov ory,cx			;
+; mov finy,cx			;los puntos finales
+; inc bx				;
+; inc bx				;en x Y y
+; mov cx,[bx]			;
+; add finx,cx			;
+; add finy,cx			;
+; xor cx,cx			;clear
+; xor dx,dx			;clear
+; mov cx,orx			;las coordenadas a dibujar deben estar en
+; mov dx,ory			;cx y dx 
+; dibuja:
+; call pixel 			;dibuja el pixel del color indicado
+; inc cx				;incrementa la direccion a dibujar en x
+; cmp cx,finx
+; jne dibuja
+; mov cx,orx
+; inc dx				;incrementa la direccion a dibujar en y
+; cmp dx,finy
+; jne dibuja
+; ret
 
-drawrec:			;funcion vacia
+; drawrec:			;funcion vacia
 
 pixel:
 push ax
@@ -106,71 +159,101 @@ pop bx
 pop ax
 ret
 
-fin:
-mov ah,10h			;espera caracter
-int 16h
-;-----------------------------------------------------------------------------
-mov cx,8			;variable para el loop
-lea bx,barrera1		;direccion de barrera1
-mov al,[bx]			;guarda valor de barrera en al
-mov ah,80h			;valor inicial para el test
-mov orx,0020h		;origen de barrera en x
-mov ory,0050h		;origen de barrera en y
+; fin:
+; mov ah,10h			;espera caracter
+; int 16h
+; ;-----------------------------------------------------------------------------
+; mov cx,8			;variable para el loop
+; lea bx,barrera1		;direccion de barrera1
+; mov al,[bx]			;guarda valor de barrera en al
+; mov ah,80h			;valor inicial para el test
+; mov orx,0020h		;origen de barrera en x
+; mov ory,0050h		;origen de barrera en y
+; again:
+; push cx
+; mov cx,orx			;origen en x en cx
+; mov dx,ory			;origen en y en dx
+; test al,ah			;test = and pero no guarda el resultado
+; je negro			
+; mov color,0Fh 		;color
+; call pixel
+; jmp sigue
+; negro:
+; mov color,0			;color
+; call pixel
+; sigue:
+; ror ah,1			;rotacion para seguir comparando
+; inc orx				;incrementa direccion en x
+; pop cx
+; loop again			;regresa
+
+; mov ah,10h			;espera caracter
+; int 16h
+
+; ;------------------------------------------------------
+; ;leer hora
+; mov ah,2ch			;instruccion para sacar la Hora
+; int 21h
+
+; mov ah,10h			;espera caracter
+; int 16h
+
+; ;--------------------------------------------------------------
+; leepixel:
+; mov ah,0Dh			;instruccion de leer pixel
+; mov bh,0			;pagina 0
+; mov cx,50			;coordenada en X 
+; mov dx,50			;coordenada en Y
+; int 10h				;color en AL			
+
+; ;--------------------------------------------------------
+; ;modo texto y set cursor
+
+; mov ah,00			;set video mode http://stanislavs.org/helppc/int_10-0.html
+; mov al,01h 			;40x25 text mode
+; int 10h   
+
+; mov ah,02h 			;set cursor position
+; mov bh,0			;pagina
+; mov dh,10			;fila
+; mov dl,20			;columna
+; int 10h
+
+; ;--------------------------------------------------------
+
+; mov ah,10h			;espera caracter
+; int 16h
+
+;--------------------------------------------------------
+dibujaimagen:
+xor cx,cx
+xor dx,dx
+lea bx,imagen1
 again:
-push cx
-mov cx,orx			;origen en x en cx
-mov dx,ory			;origen en y en dx
-test al,ah			;test = and pero no guarda el resultado
-je negro			
-mov color,0Fh 		;color
+mov al,[bx]
+mov color,al
 call pixel
-jmp sigue
-negro:
-mov color,0			;color
-call pixel
-sigue:
-ror ah,1			;rotacion para seguir comparando
-inc orx				;incrementa direccion en x
-pop cx
-loop again			;regresa
+inc cx
+inc bx
+cmp cx,imagen1x
+jne again
+xor cx,cx
+inc dx
+cmp dx,imagen1y
+jne again
 
-mov ah,10h			;espera caracter
-int 16h
-
-;------------------------------------------------------
-;leer hora
-mov ah,2ch			;instruccion para sacar la Hora
-int 21h
-
-mov ah,10h			;espera caracter
-int 16h
-
-;--------------------------------------------------------------
-leepixel:
-mov ah,0Dh			;instruccion de leer pixel
-mov bh,0			;pagina 0
-mov cx,50			;coordenada en X 
-mov dx,50			;coordenada en Y
-int 10h				;color en AL			
-
-;--------------------------------------------------------
-;modo texto y set cursor
-
-mov ah,00			;set video mode http://stanislavs.org/helppc/int_10-0.html
-mov al,01h 			;40x25 text mode
-int 10h   
-
-mov ah,02h 			;set cursor position
-mov bh,0			;pagina
-mov dh,10			;fila
-mov dl,20			;columna
-int 10h
+; cmp cx,finx
+; jne dibuja
+; mov cx,orx
+; inc dx				;incrementa la direccion a dibujar en y
+; cmp dx,finy
+; jne dibuja
 
 ;--------------------------------------------------------
 
 mov ah,10h			;espera caracter
 int 16h
-
+;--------------------------------------------------------
 mov ax,4c00h
 int 21h
 
